d19b36c8c28b2077fe3db9d71c0787ad739c3968
Kyle Clark <kyle.clark@meltmedia.com>
2014-02-12 21:16:02 -0700

    Ported kyleaclark.com from asp.net mvc into docpad structure running on node; style updates and other misc; updated project page to dynamically populate from angular controller

---
+++ src/files/js/framework/data/ProjectsData.js
@@ -0,0 +1,35 @@
+var app.ProjectsData = [
+    {
+      id: 1, title: "Mic Mac Moe Js",
+      references: [
+        { url: "http://micmacmoejs.com", name: "Website" },
+        { url: "http://micmacmoejs.com", name: "test" }
+      ],
+      descriptions: [
+        { content: "Created: 08/2013 &mdash; Last Edit: 02/2014" }
+      ],
+      technologies: [
+        { name: "JavaScript" },
+        { name: "NodeJS" },
+        { name: "Docpad" },
+        { name: "Grunt" },
+        { name: "Bower" },
+        { name: "RequireJS" },
+        { name: "CoffeeScript" },
+        { name: "Eco" },
+        { name: "jQuery" },
+        { name: "UnderscoreJS" },
+        { name: "HTML5" },
+        { name: "CSS3" }
+      ]
+    },
+    {
+      id: 2, title: "88 Depot",
+      references: [
+        { url: "http://88depot.com", name: "Website" }
+      ],
+      descriptions: [
+        { content: "Created: 2013 / Last Edit: 2014" }
+      ]
+    }
+];
